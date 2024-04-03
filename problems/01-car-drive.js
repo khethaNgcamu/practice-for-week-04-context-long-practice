0cb0a91734822b8e6a8aaa52ebd270f9d8ffd030
Smanga Ngcamu <ngcamukhetha@gamil.com>
2024-04-03 14:20:06 +0200

    context exercisess completed

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -1,4 +1,21 @@
 // Your code here
+class Car {
+	constructor(){
+		this.speed = 0;
+	}
+
+	drive(newSpeed){
+		return this.speed = newSpeed;
+	}
+}
+
+
+// let car = new Car();
+// car.drive(0);     // => returns 0
+// console.log(car)
+
+// car.drive(10);    // => returns 10
+// console.log(car);
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
--- problems/07-all-the-args.js
+++ problems/07-all-the-args.js
@@ -1,6 +1,9 @@
 function allTheArgs(func, ...args) {
   // Your code here
-}
+      return function(...callTimeArgs){
+            return func.call(this, ...args, ...callTimeArgs);
+    };
+  }
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
